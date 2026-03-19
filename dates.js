6478567689dc264d65ec460f2cf6c2111ad9ab2c
Mohit Nayak <mohitnaya0001@gmail.com>
2026-03-19 12:06:05 +0530

    date and Time

---
+++ dates.js
@@ -0,0 +1,6 @@
+let myDate = new Date();
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getFullYear());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleString())
