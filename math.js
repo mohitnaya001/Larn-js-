2878a87571d11b117b433f48522c2e3f8e45722f
Mohit Nayak <mohitnaya0001@gmail.com>
2026-03-19 11:42:48 +0530

    Math and num

---
+++ math.js
@@ -0,0 +1,11 @@
+// 
+// const score = 1000;
+
+// const zeros = 10000000;
+// console.log(zeros.toLocaleString('en-IN'));
+
+// console.log(Math.floor(10.1));
+// console.log(Math.random() * 100);
+const min = 10;
+const max =20;
+console.log(Math.floor(Math.random() * (max-min + 1))+ min)
