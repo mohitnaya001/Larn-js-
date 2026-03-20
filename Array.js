c99cec71d648f93556736701a3d4987ea3460861
Mohit Nayak <mohitnaya0001@gmail.com>
2026-03-20 14:59:26 +0530

    Array

---
+++ Array.js
@@ -0,0 +1,8 @@
+const marvel_heros =["iron-Man","Thor","lowkey","wonda"]
+const actors = ["sharukh","samlam","ranveer kapoor","katrina Kef"]
+
+// marvel_heros.push(actors)
+
+let arr = [10,20,30,40[50,40,56,46[10,43,54,64[36,635,356]]]]
+new_arr =arr.flat(5)
+console.log(new_arr);
