87bcaedffbf4d03d9358a048eefd0e9c2e892315
Mohit Nayak <mohitnaya0001@gmail.com>
2026-03-28 00:16:59 +0530

    Events in js

---
+++ Events/evanst.js
@@ -0,0 +1,21 @@
+ //const owl= document.getElementById('owl')
+    //owl.addEventListener('click',function(e){ 
+    //    console.log("click owl")
+        e.stopPropagation()
+    //},)
+    const images =document.getElementById('images')
+    images.addEventListener('click',function(e){
+    console.log("image is click")
+    })
+
+   // const google = document.querySelector('#google').addEventListener('click',function(e){
+     //   console.log('google')
+     //   e.preventDefault()
+    //    e.stopPropagation()
+   //    console.log('google')
+   // })    
+
+    const image =document.getElementById('images')
+    image.addEventListener('click',function(e){
+    console.log("hii")
+}) 
