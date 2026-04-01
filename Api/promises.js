d62a791a8c00490e48006330231785c82332eafa
Mohit Nayak <mohitnaya0001@gmail.com>
2026-04-01 21:33:15 +0530

    fetch

--- Api/promises.js
+++ Api/promises.js
@@ -1,5 +1,5 @@
 const promiseOne = new Promise(function(resolve, reject){
-    //do any async tast 
+    // do any async tast 
     // DB conected
     setTimeout(function(){
         console.log(`async task is compelete`)
@@ -49,4 +49,47 @@ promiseFour.then((user)=>{
     console.log('error')
 }).finally(()=>{
     console.log('the promis resolve and rejected')
+})
+
+const promisFive = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error = false;
+        if(!error){
+            resolve({name:`java`, type:`front`})
+        }else{
+            reject(`JS went wrong`)
+        }
+    },1000)
+})
+async function consumePromiseFive(){
+    try {
+    const response = await promisFive
+    console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+consumePromiseFive();
+
+async function GetAllUser(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data)
+
+    } catch (error) {
+        console.log('E:',error)
+    }
+}
+GetAllUser()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then ((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch(()=>{
+    
 })
